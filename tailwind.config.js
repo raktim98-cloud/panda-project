f51674d16f2102c5ad5c190eb239e5f306a8be84
raktim98-cloud <shuvrow98@gmail.com>
2025-01-08 21:10:11 +0600

    add a section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,31 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
     container: {
       center: true,
       padding: '2rem',
       
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1320px',
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
